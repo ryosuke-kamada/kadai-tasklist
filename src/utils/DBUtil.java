35034b6ef2fa2913a847ea4a2c9b1e0e93687187
ryosuke.kamada <ryosuke.kamada@growship.com>
2020-01-16 18:08:39 +0900

    Add getAllTasks

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -5,18 +5,18 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME="tasklist";
+    private static final String PERSISTENCE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
-    public static EntityManager createEntityManager(){
+    public static EntityManager createEntityManager() {
         return _getEntityManagerFactory().createEntityManager();
     }
 
-    private static EntityManagerFactory _getEntityManagerFactory(){
-        if(emf==null){
-            emf=Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    private static EntityManagerFactory _getEntityManagerFactory() {
+        if(emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
         }
+
         return emf;
     }
-
 }
